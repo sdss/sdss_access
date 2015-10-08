ca7645352f1c8eebf3b4d4399aea8eaac87afc8a
blanton <blanton@AlbireoAir.local>
2015-10-08 20:01:04 +0000

    Add access product

---
+++ README.rst
@@ -0,0 +1,5 @@
+===========
+sdss_access
+===========
+
+This repository supports access to the SDSS data set. 
---
+++ python/sdss_access/__init__.py
@@ -0,0 +1,26 @@
+# License information goes here
+# -*- coding: utf-8 -*-
+"""
+===========
+sdss_access
+===========
+
+The function sdss4tools.install.version() should be used to set the ``__version__``
+package variable.  In order for this to work properly, the svn property
+svn:keywords must be set to HeadURL on this file.
+
+.. SDSS-IV: http://trac.sdss.org
+.. Python:  http://python.org
+"""
+#
+from __future__ import absolute_import, division, print_function, unicode_literals
+# The line above will help with 2to3 support.
+from sdss4tools.install import version
+#
+# Set version string.
+#
+__version__ = version('$HeadURL$')
+#
+# Clean up namespace
+#
+del version
