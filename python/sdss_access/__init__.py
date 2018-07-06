bab2d9d3d7f5c370c3a680a4d3ed49a92a120d98
José Sánchez-Gallego <gallegoj@uw.edu>
2018-07-06 08:28:47 -0700

    Make sure config file is closed after reading it

--- python/sdss_access/__init__.py
+++ python/sdss_access/__init__.py
@@ -1,33 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+
 import yaml
+
+# set up the TREE, but match the TREE_VER if it is already there
+from tree import Tree
 
 # Inits the logging system. Only shell logging, and exception and warning catching.
 # File logging can be started by calling log.start_file_logger(name).
 from .misc import log
-
-from .path import Path as SDSSPath, AccessError
+from .path import AccessError
+from .path import Path as SDSSPath
 from .sync import HttpAccess, RsyncAccess
+
 
 NAME = 'sdss_access'
 
 # Loads config
-config = yaml.load(open(os.path.dirname(__file__) + '/etc/{0}.cfg'.format(NAME)))
+with open(os.path.dirname(__file__) + '/etc/{0}.cfg'.format(NAME)) as ff:
+    config = yaml.load(ff)
 
 
 __version__ = '0.2.5dev'
 
 
-# set up the TREE, but match the TREE_VER if it is already there
-from tree import Tree
 tree = Tree(config=os.environ['TREE_VER'] if 'TREE_VER' in os.environ else None)
 print("SDSS_ACCESS> Using %r" % tree)
-
-
