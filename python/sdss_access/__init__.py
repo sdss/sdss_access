5d1437089cc90f82cef687a90f6d5e9c9a8ab467
Michael Talbot <michaeltalbot@astro.utah.edu>
2019-03-15 19:50:19 -0600

    debugging

--- python/sdss_access/__init__.py
+++ python/sdss_access/__init__.py
@@ -21,7 +21,7 @@ tree = Tree(config=config)
 log.debug("SDSS_ACCESS> Using %r" % tree)
 
 from .path import Path as SDSSPath, AccessError
-from .sync import HttpAccess, RsyncAccess
+from .sync import HttpAccess, RsyncAccess, CurlAccess
 
 
 NAME = 'sdss_access'
