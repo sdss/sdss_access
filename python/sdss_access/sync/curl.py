b0ab7b4a7d97cbd4deadb1e5d4ce5aba1eefa771
Michael Talbot <michaeltalbot@astro.utah.edu>
2019-03-26 16:56:15 -0600

    testing why mpl4 example is not returning already downloaded when is

--- python/sdss_access/sync/curl.py
+++ python/sdss_access/sync/curl.py
@@ -207,6 +207,8 @@ class CurlAccess(SDSSPath):
                 
                 if exists(destination):
                     existing_file_size = int(popen('gzip -l %s' % destination).readlines()[1].split()[0]) if '.gz' in destination else getsize(destination)
+                    print('testing existing', existing_file_size, int(file_size), file_date, datetime.strptime(file_date, "%Y-%b-%d %H:%M" if len(file_date.split('-')[0]) == 4 else "%d-%b-%Y %H:%M"), datetime.fromtimestamp(getmtime(destination)), datetime.fromtimestamp(getmtime(destination)), 
+                    if existing_file_size == int(file_size) and abs(datetime.strptime(file_date, "%Y-%b-%d %H:%M" if len(file_date.split('-')[0]) == 4 else "%d-%b-%Y %H:%M") - datetime.fromtimestamp(getmtime(destination))).seconds < 60)
                     if existing_file_size == int(file_size) and abs(datetime.strptime(file_date, "%Y-%b-%d %H:%M" if len(file_date.split('-')[0]) == 4 else "%d-%b-%Y %H:%M") - datetime.fromtimestamp(getmtime(destination))).seconds < 60: print('Already Downloaded at %s'%destination)
                     else: yield (location, source, destination)
                 else: yield (location, source, destination)
