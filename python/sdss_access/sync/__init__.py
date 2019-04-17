6594922405295e1b039754ad09f205c41dbb50f0
Michael Talbot <michaeltalbot@astro.utah.edu>
2019-04-17 13:36:58 -0600

    fixed a syntax issue with BaseAccess

--- python/sdss_access/sync/__init__.py
+++ python/sdss_access/sync/__init__.py
@@ -2,6 +2,6 @@ from .auth import Auth
 from .cli import Cli
 from .stream import Stream
 from .http import HttpAccess
-from .sync import BaseAccess
+from .access import BaseAccess
 from .rsync import RsyncAccess
 from .curl import CurlAccess
