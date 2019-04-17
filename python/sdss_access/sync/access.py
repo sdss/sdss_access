837d47c531f90382a55e6dca9d6b8b518fefd3f2
Michael Talbot <michaeltalbot@astro.utah.edu>
2019-04-17 14:36:43 -0600

    created an Access class that can determine rsync or curl access depending on platform

--- python/sdss_access/sync/access.py
+++ python/sdss_access/sync/access.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+
 # The line above will help with 2to3 support.
 from sdss_access.sync import CurlAccess, RsyncAccess
-Base = RsyncAccess
-label = 'sdss_rsync'
+from sdss_access import is_posix
+
+Base = RsyncAccess if is_posix else CurlAccess
+label = 'sdss_rsync' if is_posix else 'sdss_curl'
 
 class Access(Base):
-
+    """Class for providing Rsync or Curl access depending on posix
+    """
     def __init__(self, label=label, stream_count=5, mirror=False, public=False, release=None, verbose=False):
         super(Base, self).__init__(mirror=mirror, public=public, release=release, verbose=verbose, label = label)
