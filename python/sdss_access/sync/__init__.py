a11fdc1b646eea3e4d12650643f495e34b3c80f0
Joel Brownstein <joelbrownstein@sdss.org>
2019-04-17 14:15:50 -0600

    access

--- python/sdss_access/sync/__init__.py
+++ python/sdss_access/sync/__init__.py
@@ -2,6 +2,7 @@ from .auth import Auth
 from .cli import Cli
 from .stream import Stream
 from .http import HttpAccess
+from .access import Access
 from .baseaccess import BaseAccess
 from .rsync import RsyncAccess
 from .curl import CurlAccess
---
+++ python/sdss_access/sync/access.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+# The line above will help with 2to3 support.
+from sdss_access.sync import CurlAccess, RsyncAccess, AccessError
+
+class Access: pass
+
+Access.__dict__ = dict(RsyncAccess.__dict__)
