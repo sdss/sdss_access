fa0db898754dc40581809ce25b1027914cf2bcff
Michael Talbot <michaeltalbot@astro.utah.edu>
2019-04-17 14:33:43 -0600

    testing

--- python/sdss_access/sync/access.py
+++ python/sdss_access/sync/access.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 # The line above will help with 2to3 support.
 from sdss_access.sync import CurlAccess, RsyncAccess
+Base = RsyncAccess
+label = 'sdss_rsync'
 
-class Access: pass
+class Access(Base):
 
-Access.__dict__ = dict(RsyncAccess.__dict__)
+    def __init__(self, label=label, stream_count=5, mirror=False, public=False, release=None, verbose=False):
+        super(Base, self).__init__(mirror=mirror, public=public, release=release, verbose=verbose, label = label)
