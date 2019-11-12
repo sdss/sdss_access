961a63f248201604342b6b04ec2473f4387696ee
Brian Cherinka <havok2063@hotmail.com>
2019-11-12 14:52:24 -0500

    fixing importlib in test for py2.7

--- python/sdss_access/tests/sync/test_access.py
+++ python/sdss_access/tests/sync/test_access.py
@@ -7,7 +7,7 @@
 # Created: Thursday, 8th August 2019 6:08:07 pm
 # License: BSD 3-clause "New" or "Revised" License
 # Copyright (c) 2019 Brian Cherinka
-# Last Modified: Friday, 18th October 2019 4:25:00 pm
+# Last Modified: Tuesday, 12th November 2019 2:51:10 pm
 # Modified By: Brian Cherinka
 
 
@@ -15,7 +15,11 @@ from __future__ import print_function, division, absolute_import
 import pytest
 from sdss_access.sync import CurlAccess, RsyncAccess
 import sdss_access
-import importlib
+try:
+    import importlib
+except ImportError:
+    import imp as importlib
+
 
 is_posix = [True, False]
 exp = {True: (RsyncAccess, 'rsync'), False: (CurlAccess, 'curl')}
