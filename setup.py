ed16c5eca6715323b48644cf61db7a7aab00d06e
Brian Cherinka <havok2063@hotmail.com>
2020-04-28 11:09:09 -0400

    simplifying to setup.cfg

--- setup.py
+++ setup.py
@@ -1,101 +1,103 @@
-#!/usr/bin/env python
 # encoding: utf-8
 #
 # setup.py
 #
 
+from setuptools import setup
 
-from __future__ import division
-from __future__ import print_function
-from __future__ import absolute_import
-from setuptools import setup, find_packages
+setup()
 
-import os
-import argparse
-import sys
+# from __future__ import division
+# from __future__ import print_function
+# from __future__ import absolute_import
+# from setuptools import setup, find_packages
 
-NAME = 'sdss-access'
-VERSION = '1.0.0dev'
-RELEASE = 'dev' in VERSION
+# import os
+# import argparse
+# import sys
 
-
-def run(packages, install_requires):
-
-    setup(name=NAME,
-          version=VERSION,
-          license='BSD3',
-          description='package to access all SDSS SAS products',
-          long_description=open('README.rst').read(),
-          author='Michael Blanton',
-          author_email='michael.blanton@nyu.edu',
-          keywords='astronomy software',
-          url='https://github.com/sdss/sdss_access',
-          include_package_data=True,
-          packages=packages,
-          install_requires=install_requires,
-          package_dir={'': 'python'},
-          scripts=[],
-          classifiers=[
-              'Development Status :: 4 - Beta',
-              'Intended Audience :: Science/Research',
-              'License :: OSI Approved :: BSD License',
-              'Natural Language :: English',
-              'Operating System :: OS Independent',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 2.6',
-              'Programming Language :: Python :: 2.7',
-              'Topic :: Documentation :: Sphinx',
-              'Topic :: Software Development :: Libraries :: Python Modules',
-          ],
-          )
+# NAME = 'sdss-access'
+# VERSION = '1.0.0dev'
+# RELEASE = 'dev' in VERSION
 
 
-def get_requirements(opts):
-    ''' Get the proper requirements file based on the optional argument '''
+# def run(packages, install_requires):
 
-    if opts.dev:
-        name = 'requirements_dev.txt'
-    elif opts.doc:
-        name = 'requirements_doc.txt'
-    else:
-        name = 'requirements.txt'
+#     setup(name=NAME,
+#           version=VERSION,
+#           license='BSD3',
+#           description='package to access all SDSS SAS products',
+#           long_description=open('README.rst').read(),
+#           author='Michael Blanton',
+#           author_email='michael.blanton@nyu.edu',
+#           keywords='astronomy software',
+#           url='https://github.com/sdss/sdss_access',
+#           include_package_data=True,
+#           packages=packages,
+#           install_requires=install_requires,
+#           package_dir={'': 'python'},
+#           scripts=[],
+#           classifiers=[
+#               'Development Status :: 4 - Beta',
+#               'Intended Audience :: Science/Research',
+#               'License :: OSI Approved :: BSD License',
+#               'Natural Language :: English',
+#               'Operating System :: OS Independent',
+#               'Programming Language :: Python',
+#               'Programming Language :: Python :: 2.6',
+#               'Programming Language :: Python :: 2.7',
+#               'Topic :: Documentation :: Sphinx',
+#               'Topic :: Software Development :: Libraries :: Python Modules',
+#           ],
+#           )
 
-    requirements_file = os.path.join(os.path.dirname(__file__), name)
-    install_requires = [line.strip().replace('==', '>=') for line in open(requirements_file)
-                        if not line.strip().startswith('#') and line.strip() != '']
-    return install_requires
+
+# def get_requirements(opts):
+#     ''' Get the proper requirements file based on the optional argument '''
+
+#     if opts.dev:
+#         name = 'requirements_dev.txt'
+#     elif opts.doc:
+#         name = 'requirements_doc.txt'
+#     else:
+#         name = 'requirements.txt'
+
+#     requirements_file = os.path.join(os.path.dirname(__file__), name)
+#     install_requires = [line.strip().replace('==', '>=') for line in open(requirements_file)
+#                         if not line.strip().startswith('#') and line.strip() != '']
+#     return install_requires
 
 
-def remove_args(parser):
-    ''' Remove custom arguments from the parser '''
+# def remove_args(parser):
+#     ''' Remove custom arguments from the parser '''
 
-    arguments = []
-    for action in list(parser._get_optional_actions()):
-        if '--help' not in action.option_strings:
-            arguments += action.option_strings
+#     arguments = []
+#     for action in list(parser._get_optional_actions()):
+#         if '--help' not in action.option_strings:
+#             arguments += action.option_strings
 
-    for arg in arguments:
-        if arg in sys.argv:
-            sys.argv.remove(arg)
+#     for arg in arguments:
+#         if arg in sys.argv:
+#             sys.argv.remove(arg)
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    # Custom parser to decide whether which requirements to install
-    parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
-    parser.add_argument('-d', '--dev', dest='dev', default=False, action='store_true', help='Install all packages for development')
-    parser.add_argument('-o', '--doc', dest='doc', default=False, action='store_true', help='Install only core + documentation packages')
+#     # Custom parser to decide whether which requirements to install
+#     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]))
+#     parser.add_argument('-d', '--dev', dest='dev', default=False, action='store_true', help='Install all packages for development')
+#     parser.add_argument('-o', '--doc', dest='doc', default=False, action='store_true', help='Install only core + documentation packages')
 
-    # We use parse_known_args because we want to leave the remaining args for distutils
-    args = parser.parse_known_args()[0]
+#     # We use parse_known_args because we want to leave the remaining args for distutils
+#     args = parser.parse_known_args()[0]
 
-    # Get the proper requirements file
-    install_requires = get_requirements(args)
+#     # Get the proper requirements file
+#     install_requires = get_requirements(args)
 
-    # Now we remove all our custom arguments to make sure they don't interfere with distutils
-    remove_args(parser)
+#     # Now we remove all our custom arguments to make sure they don't interfere with distutils
+#     remove_args(parser)
 
-    # Have distutils find the packages
-    packages = find_packages(where='python')
+#     # Have distutils find the packages
+#     packages = find_packages(where='python')
 
-    # Runs distutils
-    run(packages, install_requires)
+#     # Runs distutils
+#     run(packages, install_requires)
